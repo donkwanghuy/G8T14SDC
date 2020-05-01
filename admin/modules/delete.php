9186a12650330085e78ef0cb64817c9c4f58390d
namtt2901 <62321074+namtt2901@users.noreply.github.com>
2020-05-01 10:21:31 +0700

    Delete delete.php

--- admin/modules/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-$code = $_GET["ma"];
-include("include/open.php");
-$strSelect = "delete from sp where ma_sp = $code";
-mysqli_query($conn,$strSelect);
-include("include/close.php");
-header("Location:list.php");
-?>
